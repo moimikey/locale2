9d201990e9a242216bfab131098593523a45716b
Michael Scott Hertzberg <mshertzberg@gmail.com>
2015-04-19 23:00:35 -0400

    [beta] get node environment locale if not in-browser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = (function() {
-  return ((global.clientInformation || global.navigator || Object.create(null)).language) ||
-  global.navigator && (global.navigator.userLanguage || global.navigator.languages[1])
+  return ((global.clientInformation || global.navigator || Object.create(null)).language)
+  || global.navigator && (global.navigator.userLanguage || global.navigator.languages[1])
+  || (locale = process.env.LANG || process.env.LC_CTYPE) && locale.split('.')[0].replace('_','-')
   || undefined
 })();
