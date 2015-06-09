d7a20e08144be8793f96affbfc24e4b3d9bd0e7b
Michael Scott Hertzberg <mhertzberg@shopkeep.com>
2015-06-09 11:00:18 -0400

    minor fixes. bump to stable v0.1.0

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,7 @@
 var test = require('tape');
-var Locale = require('..');
 
 test('it gets a locale', function (t) {
-  t.plan(2);
+  var Locale = require('..');
+  t.plan(1);
   t.equal(typeof Locale, 'string');
-  t.equal(Locale, new RegExp('^[a-z]{2}-[A-Za-z]{2}$/'));
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = (function() {
   return ((global.clientInformation || global.navigator || Object.create(null)).language)
-  || global.navigator && (global.navigator.userLanguage || global.navigator.languages[1])
+  || global.navigator && (global.navigator.userLanguage ||
+                          global.navigator.languages[1] ||
+                          global.navigator.match(/;.(\w+\-\w+)/i)[1])
   || (locale = process.env.LANG || process.env.LC_CTYPE) && locale.split('.')[0].replace('_','-')
   || undefined
 })();
