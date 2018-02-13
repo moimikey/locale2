a3d76f4a4a3abb41dd2b0da76954d57018e0beaf
James G. Kim <jgkim@jayg.org>
2018-02-13 23:34:41 +0900

    Fix the priority
    - In some circumstances, `window.navigator` is more accurate than `Intl.DateTimeFormat()`.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ var formatLocale = require('./utils').formatLocale
 function getLocale(locale) {
   if (locale) return locale
 
-  if (global.Intl && typeof global.Intl.DateTimeFormat === 'function') {
-    return global.Intl.DateTimeFormat().resolvedOptions && global.Intl.DateTimeFormat().resolvedOptions().locale
-  }
-
   if (global.chrome && global.chrome.app && typeof global.chrome.app.getDetails === 'function') {
     locale = global.chrome.app.getDetails()
 
@@ -21,11 +17,17 @@ function getLocale(locale) {
              (global.navigator.languages && global.navigator.languages[0]) ||
              (global.navigator.userAgent && global.navigator.userAgent.match(/;.(\w+\-\w+)/i)[1])))
 
-  if (!locale && ['LANG', 'LANGUAGE'].some(Object.hasOwnProperty, process.env)) {
-    return (process.env.LANG ||
-            process.env.LANGUAGE ||
-            String()).replace(/[.:].*/, '')
-                     .replace('_', '-')
+  if (!locale) {
+    if (global.Intl && typeof global.Intl.DateTimeFormat === 'function') {
+      locale = global.Intl.DateTimeFormat().resolvedOptions && global.Intl.DateTimeFormat().resolvedOptions().locale
+    }
+    
+    if (!locale && ['LANG', 'LANGUAGE'].some(Object.hasOwnProperty, process.env)) {
+      return (process.env.LANG ||
+              process.env.LANGUAGE ||
+              String()).replace(/[.:].*/, '')
+                       .replace('_', '-')
+    }
   }
 
   return locale
